3dabd1adc210cb670bed3a0543a9d4acc1317f43
Juliana Kang <julianajlk@gmail.com>
2018-07-16 01:47:39 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -167,6 +167,19 @@ def player_numbers(team_name)
     player[:number]
 end
 
-def player_stats
+def player_stats(player_name)
+  player_stats={}
+  game_hash.each do |team, team_data|
+    team_data[:players].each do |stats|
+      if stats[:name] == player_name
+        stats.delete(:name)
+        player_stats = stats
+      end
+    end
+  end
+  player_stats
+end
+
+def big_shoe_rebounds
 
 end
